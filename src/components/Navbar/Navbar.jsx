7fc32d15cf597d832fcac44f83cbfc7d48251717
Fabien Chareun <cacrfabfr@gmail.com>
2022-04-18 00:52:33 +0200

    navbar setup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,29 @@
 import './Navbar.scss';
 import logo from '../../assets/shared/logo.svg';
+import burgerLogo from '../../assets/shared/icon-hamburger.svg'
+import burgerLogoClose from '../../assets/shared/icon-close.svg'
+import { useState } from 'react';
+
+const Menu = () => (
+  <ul>
+    <li className="uppercase">
+      <span className="numbers">00</span> Home
+    </li>
+    <li className="uppercase">
+      <span className="numbers">01</span> Destination
+    </li>
+    <li className="uppercase">
+      <span className="numbers">02</span> Crew
+    </li>
+    <li className="uppercase">
+      <span className="numbers">03</span> Technology
+    </li>
+  </ul>
+);
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="navbar__container">
       <div className="navbar__logo">
@@ -11,20 +33,21 @@ const Navbar = () => {
       <div className="navbar__line"></div>
 
       <div className="navbar__links">
-        <ul>
-          <li className="uppercase">
-            <span className="numbers">00</span> Home
-          </li>
-          <li className="uppercase">
-            <span className="numbers">01</span> Destination
-          </li>
-          <li className="uppercase">
-            <span className="numbers">02</span> Crew
-          </li>
-          <li className="uppercase">
-            <span className="numbers">03</span> Technology
-          </li>
-        </ul>
+        <Menu />
+      </div>
+      <div className="navbar__links-smallcreen">
+        {toggleMenu 
+          ? <img src={burgerLogoClose} alt="menu icon" onClick={() => setToggleMenu(false)} />
+          : <img src={burgerLogo} alt="menu icon" onClick={() => setToggleMenu(true)} />
+        }
+
+        {toggleMenu && (
+          <div className="navbar__links-smallscreen_menu slide-left">
+            <Menu />
+          </div>
+
+        )}
+
       </div>
     </div>
   );
