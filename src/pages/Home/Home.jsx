b92410cacb3ab8a0740d1ffbec197208cffe7930
Fabien Chareun <cacrfabfr@gmail.com>
2022-04-18 13:29:16 +0200

    home desktop done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,27 @@
-import './Home.scss'
+import { Link } from 'react-router-dom';
+import './Home.scss';
 
 const Home = () => {
   return (
     <div className="home__container">
-      <h1>Home</h1>
+      <div className="home__content-left">
+        <h5 className="uppercase">So, you want to travel to</h5>
+        <h1 className="uppercase">Space</h1>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+
+      <div className="home__content-right">
+        <Link to="/destination">
+          <div className="button__home uppercase">Explore</div>
+        </Link>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
