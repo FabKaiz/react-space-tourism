e4d55d60b7da7e02278506fbf89977dc507029e4
Fabien Chareun <cacrfabfr@gmail.com>
2022-04-19 13:16:13 +0200

    destination setup and base style

--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -10,17 +10,33 @@ const Destination = () => {
 
   return (
     <div className="destination__container background__img">
-      <h5 className="uppercase">
-        <span>01</span> Pick your destination
-      </h5>
       <div className="destination__content-container">
         <div className="destination__content-left">
+          <h5 className="uppercase">
+            <span>01</span> Pick your destination
+          </h5>
           <img src={images.png} alt={name} />
         </div>
         <div className="destination__content-right">
           <nav>
             <li>{name}</li>
           </nav>
+
+          <h1 className="uppercase">{name}</h1>
+          <p className="description">{description}</p>
+
+          <hr />
+
+          <div className="info">
+            <div>
+              <p className="subheading2 uppercase">Avg. Distance</p>
+              <p className="subheading1 uppercase">{distance}</p>
+            </div>
+            <div>
+              <p className="subheading2 uppercase">Est. travel time</p>
+              <p className="subheading1 uppercase">{travel}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
