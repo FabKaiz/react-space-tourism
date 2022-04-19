ab471e791f4dad321b3e16d890db66b166325d41
Fabien Chareun <cacrfabfr@gmail.com>
2022-04-19 12:25:37 +0200

    updated foler architecture

--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Destination.scss';
+import Data from '../../data/data.json';
 
 const Destination = () => {
+  const [destinations] = useState(Data.destinations);
+  const [value, setValue] = useState(0);
+
+  const { name, images, description, distance, travel } = destinations[value];
+
   return (
     <div className="destination__container background__img">
-      <div className="destination__content-left">
-     
-      </div>
-
-      <div className="destination__content-right">
-
+      <h5 className="uppercase">
+        <span>01</span> Pick your destination
+      </h5>
+      <div className="destination__content-container">
+        <div className="destination__content-left">
+          <img src={images.png} alt={name} />
+        </div>
+        <div className="destination__content-right">
+          <nav>
+            <li>{name}</li>
+          </nav>
+        </div>
       </div>
     </div>
   );
