1dc8c07302b7f6e5137158259fe4c271ad107c0f
Fabien Chareun <cacrfabfr@gmail.com>
2022-04-19 15:14:08 +0200

    framer motion routing setup

---
+++ src/components/AnimatedRoutes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Home from '../pages/Home/Home';
+import Destination from '../pages/Destination/Destination'
+import Crew from '../pages/Crew/Crew'
+import Technology from '../pages/Technology/Technology'
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+
+  return (
+    <Routes location={location} key={location.pathname}>
+      <Route path="/" element={<Home />} />
+      <Route path="/destination" element={<Destination />} />
+      <Route path="/crew" element={<Crew />} />
+      <Route path="/technology" element={<Technology />} />
+    </Routes>
+  );
+};
+
+export default AnimatedRoutes;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import Home from './pages/Home/Home';
 import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Destination from './pages/Destination/Destination';
-import Crew from './pages/Crew/Crew';
-import Technology from './pages/Technology/Technology';
+import { BrowserRouter } from 'react-router-dom';
+
+import AnimatedRoutes from './components/AnimatedRoutes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/destination" element={<Destination />} />
-        <Route path="/crew" element={<Crew />} />
-        <Route path="/technology" element={<Technology />} />
-      </Routes>
+      <AnimatedRoutes />
     </BrowserRouter>
   </React.StrictMode>
 );
