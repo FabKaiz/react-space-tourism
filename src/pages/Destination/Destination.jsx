d6ee0b373102234793d03921cdac1306dcf1607d
Fabien Chareun <cacrfabfr@gmail.com>
2022-04-20 15:25:20 +0200

    added ReactTransitionGroup animation

--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './Destination.scss';
 import Data from '../../data/data.json';
 
 import { motion } from 'framer-motion';
+import { CSSTransition } from 'react-transition-group';
+import { SwitchTransition } from 'react-transition-group';
 
 const Destination = () => {
   const [destinations] = useState(Data.destinations);
   const [value, setValue] = useState(0);
+  const nodeRefRight = useRef(null);
+  const nodeRefLeft = useRef(null);
 
   const { name, images, description, distance, travel } = destinations[value];
 
@@ -18,13 +22,26 @@ const Destination = () => {
       transition={{ duration: 0.5 }}
     >
       <div className="destination__container background__img">
+        <h5 className="uppercase">
+          <span>01</span> Pick your destination
+        </h5>
+
         <div className="destination__content-container">
           <div className="destination__content-left">
-            <h5 className="uppercase">
-              <span>01</span> Pick your destination
-            </h5>
-            <img src={images.webp} alt={name} />
+            <SwitchTransition>
+              <CSSTransition
+                nodeRef={nodeRefLeft}
+                key={value}
+                timeout={400}
+                classNames="fade-img"
+              >
+                <div ref={nodeRefLeft}>
+                  <img src={images.webp} alt={name} />
+                </div>
+              </CSSTransition>
+            </SwitchTransition>
           </div>
+
           <div className="destination__content-right">
             <nav>
               {destinations.map((destination, index) => (
@@ -37,19 +54,30 @@ const Destination = () => {
                 </li>
               ))}
             </nav>
-            <h1 className="uppercase">{name}</h1>
-            <p className="description">{description}</p>
-            <hr />
-            <div className="info">
-              <div>
-                <p className="subheading2 uppercase">Avg. Distance</p>
-                <p className="subheading1 uppercase">{distance}</p>
-              </div>
-              <div>
-                <p className="subheading2 uppercase">Est. travel time</p>
-                <p className="subheading1 uppercase">{travel}</p>
-              </div>
-            </div>
+            <SwitchTransition>
+              <CSSTransition
+                nodeRef={nodeRefRight}
+                key={value}
+                timeout={500}
+                classNames="slide"
+              >
+                <div ref={nodeRefRight}>
+                  <h1 className="uppercase">{name}</h1>
+                  <p className="description">{description}</p>
+                  <hr />
+                  <div className="info">
+                    <div>
+                      <p className="subheading2 uppercase">Avg. Distance</p>
+                      <p className="subheading1 uppercase">{distance}</p>
+                    </div>
+                    <div>
+                      <p className="subheading2 uppercase">Est. travel time</p>
+                      <p className="subheading1 uppercase">{travel}</p>
+                    </div>
+                  </div>
+                </div>
+              </CSSTransition>
+            </SwitchTransition>
           </div>
         </div>
       </div>
